accc91d6c5e81f8f1ffe9e639c8c933c900efe3a
Nikita Shabashov <niksh81@gmail.com>
2022-11-06 15:27:08 +0400

    wip on ruletka v2

--- src/main/kotlin/com/csruletka/controller/RuletkaController.kt
+++ src/main/kotlin/com/csruletka/controller/RuletkaController.kt
@@ -3,6 +3,7 @@ package com.csruletka.controller
 import com.csruletka.dto.games.ruletka.RuletkaHistory
 import com.csruletka.dto.user.UserItemToAddDto
 import com.csruletka.service.GameService
+import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Post
@@ -17,11 +18,14 @@ class RuletkaController(
     private val gameService: GameService
 ) {
     @Post("skin")
-    suspend fun addSkins(principal: Principal, skins: List<UserItemToAddDto>) {
+    suspend fun addSkins(
+        @Body
+        skins: List<UserItemToAddDto>,
+        principal: Principal
+    ) {
         gameService.ruletkaAddSkins(principal.name, skins)
     }
 
     @Get("history")
     fun getHistory(principal: Principal): Flux<RuletkaHistory> = gameService.getRuletkaHistory()
-
 }
